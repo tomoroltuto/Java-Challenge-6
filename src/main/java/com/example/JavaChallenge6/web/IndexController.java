239a9a6e05b63a5577250f2c5e63fd2cf4409562
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-08-23 00:55:18 +0900

    Web上にHello World+日時が表示できるように追加

---
+++ src/main/java/com/example/JavaChallenge6/web/IndexController.java
@@ -0,0 +1,23 @@
+package com.example.JavaChallenge6.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String index(Model model) {
+        LocalDateTime today = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyy年MM月dd日 HH時mm分ss秒");
+        String dateTimeJa = dateTimeFormatter.format(today);
+
+        model.addAttribute("today", dateTimeJa);
+        return "index";
+    }
+}
