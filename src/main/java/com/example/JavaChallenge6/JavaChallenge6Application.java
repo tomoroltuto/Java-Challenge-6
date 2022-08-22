a6d007ec1f604e48243d11ace89eed9f70e2af8a
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-08-22 23:15:40 +0900

    first commit

---
+++ src/main/java/com/example/JavaChallenge6/JavaChallenge6Application.java
@@ -0,0 +1,13 @@
+package com.example.JavaChallenge6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChallenge6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChallenge6Application.class, args);
+	}
+
+}
